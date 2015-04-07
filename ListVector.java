d63eb9c309a4e29b63725f13ce3e31d3a2890e0e
demoss1306 <demoss@tut.by>
2015-04-07 10:35:08 +0300

    Create ListVector.java

---
+++ ListVector.java
@@ -0,0 +1,19 @@
+package boilerplate.vector.third;
+
+public class ListVector extends Vector {
+    public ListVector(int size) {
+        super();
+    }
+
+    public ListVector(double[] elements) {
+        super();
+    }
+
+    public void insert(int i, int i1) {
+
+    }
+
+    public double remove(int i) {
+        return 0;
+    }
+}
