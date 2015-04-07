b915b574416a81f8d5a91defcd2270961337fddc
demoss1306 <demoss@tut.by>
2015-04-07 10:36:09 +0300

    Create ArrayVector.java

---
+++ ArrayVector.java
@@ -0,0 +1,11 @@
+package boilerplate.vector.third;
+
+public class ArrayVector extends Vector {
+    public ArrayVector(int size) {
+        super();
+    }
+
+    public ArrayVector(double[] elements) {
+        super();
+    }
+}
