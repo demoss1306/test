5f9f55bd55df6855a5c762a5041926910fd01527
demoss1306 <demoss@tut.by>
2015-04-07 10:34:16 +0300

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,1 +1,47 @@
+package boilerplate.vector.third;
 
+public class Vector {
+    public int size() {
+        return 0;
+    }
+
+    public double get(int i) {
+        return 0;
+    }
+
+    public void set(int i, int i1) {
+
+    }
+
+    public double minimum() {
+        return 0;
+    }
+
+    public double maximum() {
+        return 0;
+    }
+
+    public void sort() {
+
+    }
+
+    public int indexOf(int i) {
+        return 0;
+    }
+
+    public double average() {
+        return 0;
+    }
+
+    public void add(Vector vector) {
+
+    }
+
+    public void subtract(Vector vector) {
+
+    }
+
+    public void multiply(Vector vector) {
+
+    }
+}
